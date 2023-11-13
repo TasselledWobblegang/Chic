423b2cbb06b710ae5eddda582f0d8cda3e1dbe5e
GiovanniCortes <giocortes19@gmail.com>
2023-11-12 22:36:36 -0500

    installed dotenv and created test route for post request to /images endpoint for image upload

--- server/server.js
+++ server/server.js
@@ -5,17 +5,24 @@ const PORT = 3000;
 const app = express();
 app.use(express.json());
 
-const authRouter = require('./routes/auth.js')
+const authRouter = require('./routes/auth.js');
 
 // statically serve everything in the build folder on the route '/build'
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 
 app.use('/auth', authRouter);
 
+// TEST: POSTING IMAGES
+app.post('/images', (req, res) => {
+  res.send('sent an image! 🔥');
+});
+
 app.get('/*', (req, res) => {
   return res.sendFile(path.join(__dirname, '../dist', '../index.html'));
 });
 
-app.listen(PORT, () => { console.log(`listening on port ${PORT}`) });
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
 
 module.exports = app;
