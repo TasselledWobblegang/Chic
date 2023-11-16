071ee6354291db5a521d8383d97089fa710c61c3
jgotts89 <jeff.gottstein@gmail.com>
2023-11-16 10:14:02 -0500

    "set up some testing files"

---
+++ tests/signup.test.jsx
@@ -0,0 +1,15 @@
+//test that username and password are strings?
+//test for signup handler
+const request = require('supertest');
+
+describe('Signup test', () => {
+
+})
+
+describe('signupHandler test', () => {
+
+})
+
+describe('return statement test', () => {
+
+})
